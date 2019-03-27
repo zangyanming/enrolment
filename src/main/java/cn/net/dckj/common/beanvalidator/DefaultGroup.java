6b50151a9a8d4ba6cf8f7c9dff3bc9aec120901e
zang <1>
2019-03-27 14:10:11 +0800

    init

--- src/main/java/cn/net/dckj/common/beanvalidator/DefaultGroup.java
+++
@@ -1,9 +0,0 @@
-package cn.net.dckj.common.beanvalidator;
-
-/**
- * 默认Bean验证组
- * @author ZYM
- */
-public interface DefaultGroup {
-
-}
--- src/main/java/cn/net/dckj/common/beanvalidator/EditGroup.java
+++
@@ -1,9 +0,0 @@
-package cn.net.dckj.common.beanvalidator;
-
-/**
- * 编辑Bena验证组
- * @author ZYM
- */
-public interface EditGroup {
-
-}
